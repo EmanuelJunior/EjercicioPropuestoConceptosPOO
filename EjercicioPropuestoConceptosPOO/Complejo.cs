556dabca946fd21d6a55629935096b22266fcece
Emanuel Garcés <sagazu13@gmail.com>
2022-08-30 20:54:21 -0500

    Agregar archivos de proyecto.

---
+++ EjercicioPropuestoConceptosPOO/Complejo.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EjercicioPropuestoConceptosPOO
+{
+    class Complejo
+    {
+        double realNum, imaginaryNum;
+
+        public double RealNum { get => realNum; set => realNum = value; }
+        public double ImaginaryNum { get => imaginaryNum; set => imaginaryNum = value; }
+
+        public Complejo()
+        {
+            realNum = 0;
+            imaginaryNum = 0;
+        }
+
+        public Complejo( double realNum, double imaginaryNum )
+        {
+            this.realNum = realNum;
+            this.imaginaryNum = imaginaryNum;
+        }
+
+        public override string ToString()
+        {
+            return $"Imaginary: { imaginaryNum }, Int: { realNum }";
+        }
+
+        public static Complejo operator +( Complejo PI, Complejo SI )
+        {
+            double e = PI.RealNum + SI.RealNum;
+            double i = PI.ImaginaryNum + SI.ImaginaryNum;
+
+            return new Complejo( e, i );
+        }
+
+        public static Complejo operator -( Complejo PI, Complejo SI )
+        {
+            double e = PI.RealNum - SI.RealNum;
+            double i = PI.ImaginaryNum - SI.ImaginaryNum;
+
+            return new Complejo( e, i );
+        }
+
+        public static Complejo operator *( Complejo PI, Complejo SI )
+        {
+            double e = ( PI.RealNum * SI.RealNum ) - ( PI.ImaginaryNum * SI.ImaginaryNum);
+            double i = ( PI.RealNum * SI.ImaginaryNum) - ( PI.ImaginaryNum * SI.RealNum );
+
+            return new Complejo(e, i);
+        }
+
+        public static Complejo operator *( Complejo PI, double n )
+        {
+            double e = PI.RealNum * n;
+            double i = PI.ImaginaryNum * n;
+
+            return new Complejo(e, i);
+        }
+
+        public static Complejo operator /( Complejo PI, Complejo SI )
+        {
+            double e = ((PI.RealNum * SI.RealNum) / (Math.Pow(SI.RealNum, 2) + Math.Pow(SI.ImaginaryNum, 2)));
+            double i = (PI.ImaginaryNum * SI.RealNum - PI.RealNum * SI.ImaginaryNum) / ((Math.Pow(SI.RealNum, 2) + Math.Pow(SI.ImaginaryNum, 2)));
+
+            return new Complejo(e, i);
+        }
+
+    }
+}
---
+++ EjercicioPropuestoConceptosPOO/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EjercicioPropuestoConceptosPOO
+{
+    class Program
+    {
+        static void Main()
+        {
+            Complejo PrimerInstancia = new Complejo( 5, 3 );
+            Complejo SegundaInstancia = new Complejo( 2, 6 );
+
+            Console.WriteLine( $"Suma: { PrimerInstancia + SegundaInstancia }" );
+            Console.WriteLine( $"Resta: { PrimerInstancia + SegundaInstancia }" );
+            Console.WriteLine( $"Multiplicación: { PrimerInstancia + SegundaInstancia }" );
+            Console.WriteLine( $"Multiplicación con un Double: { PrimerInstancia * 5.5 }" );
+            Console.WriteLine( $"División: { PrimerInstancia / SegundaInstancia }" );
+        }
+    }
+}
